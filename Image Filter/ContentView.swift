c2a09797ae81579064b0a5fed3569ebfa09dce68
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2026-01-13 00:49:45 -0300

    Initial Commit

---
+++ Image Filter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Image Filter
+//
+//  Created by Isaque da Silva on 1/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Image Filter/Image_FilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_FilterApp.swift
+//  Image Filter
+//
+//  Created by Isaque da Silva on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Image_FilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
