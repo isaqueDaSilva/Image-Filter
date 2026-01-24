99166342d09bde88863ce754db3f0eee62b3b506
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2026-01-24 03:43:24 -0300

    feat - creating a default method at the vImage_Buffer to automaticaly recreate the cg image.

--- Image Filter/Extensions/vImage_Buffer+Extension.swift
+++ Image Filter/Extensions/vImage_Buffer+Extension.swift
@@ -5,4 +5,22 @@
 //  Created by Isaque da Silva on 1/24/26.
 //
 
-import Foundation
+import Accelerate
+import SwiftUI
+
+extension vImage_Buffer {
+    func recreateImage(with imageFormat: vImage_CGImageFormat) throws(ImageRepresentableError) -> DefaultImage {
+        do {
+            let newImage = try self.createCGImage(format: imageFormat)
+            self.free()
+            #if canImport(UIKit)
+            return .init(cgImage: newImage)
+            #elseif canImport(AppKit)
+            return .init(cgImage: newImage, size: self.size)
+            #endif
+        } catch {
+            print(error.localizedDescription)
+            throw .failedToCreateCGImage
+        }
+    }
+}
