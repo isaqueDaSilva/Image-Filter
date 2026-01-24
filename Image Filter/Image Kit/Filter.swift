ea7236237e25ec02a3f91c2b6397b0045c1ba126
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2026-01-24 03:45:26 -0300

    cleanup - removing unnecessary code to process color invert

--- Image Filter/Image Kit/Filter.swift
+++ Image Filter/Image Kit/Filter.swift
@@ -12,50 +12,20 @@ import SwiftUI
 struct Filter { }
 
 extension Filter {
-    enum Negative {
+    enum ColorInversion {
         static func apply(at image: DefaultImage) async throws(ImageRepresentableError) -> DefaultImage {
             var buffer = try image.getPixelBuffer()
-            processingNegative(buffer: &buffer)
+            invertColors(buffer: &buffer)
             
-            let imageFormat = try buildFormat()
-            
-            return try createImage(from: buffer, and: imageFormat)
+            return try buffer.recreateImage(with: .rgbFormat)
         }
         
-        private static func buildFormat() throws(ImageRepresentableError) -> vImage_CGImageFormat {
-            guard let imageFormat = vImage_CGImageFormat(
-                bitsPerComponent: 8,
-                bitsPerPixel: 8 * 4,
-                colorSpace: CGColorSpace(name: CGColorSpace.displayP3)!,
-                bitmapInfo: .init(rawValue: CGImageAlphaInfo.noneSkipFirst.rawValue))
-            else {
-                throw .formatterNotAvailable
-            }
-            
-            return imageFormat
-        }
-        
-        private static func processingNegative(buffer: inout vImage_Buffer) {
+        private static func invertColors(buffer: inout vImage_Buffer) {
             let height = Int32(buffer.height)
             let width = Int32(buffer.width)
             let rowBytes = Int32(buffer.rowBytes)
             
-            apply_negative(buffer.data, height, width, rowBytes)
-        }
-        
-        private static func createImage(from buffer: vImage_Buffer, and imageFormat: vImage_CGImageFormat) throws(ImageRepresentableError) -> DefaultImage {
-            do {
-                let negativeImage = try buffer.createCGImage(format: imageFormat)
-                buffer.free()
-                #if canImport(UIKit)
-                return .init(cgImage: negativeImage)
-                #elseif canImport(AppKit)
-                return .init(cgImage: negativeImage, size: buffer.size)
-                #endif
-            } catch {
-                print(error.localizedDescription)
-                throw .failedToCreateCGImage
-            }
+            ImageKit.apply_inverse(buffer.data, height, width, rowBytes)
         }
     }
 }
