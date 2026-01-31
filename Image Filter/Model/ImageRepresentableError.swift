4a7db8c35f6272f4b9929d4c925af1bc9e0b2b52
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2026-01-31 01:48:49 -0300

    feat - add more error cases

--- Image Filter/Model/ImageRepresentableError.swift
+++ Image Filter/Model/ImageRepresentableError.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum ImageRepresentableError: Error {
-    case importFailed, convertionFailed, formatterNotAvailable, failedToCreateCGImage
+    case importFailed, convertionFailed, formatterNotAvailable, failedToCreateCGImage, failedToSaveImage, photoLibraryAccessDenied
     
     var title: String {
         switch self {
@@ -16,6 +16,10 @@ enum ImageRepresentableError: Error {
             "Failed to import Image"
         case .convertionFailed, .formatterNotAvailable, .failedToCreateCGImage:
             "Internal error"
+        case .failedToSaveImage:
+            "Failed to save image"
+        case .photoLibraryAccessDenied:
+            "Photo Library Access Denied"
         }
     }
     
@@ -25,6 +29,10 @@ enum ImageRepresentableError: Error {
             "The image you selected from your library did not load correctly. Please select it again or contact us."
         case .convertionFailed, .formatterNotAvailable, .failedToCreateCGImage:
             "An internal error happened. Please try to repeat the action or contact us."
+        case .failedToSaveImage:
+            "Failed to save the edited image. Please try again or contact us."
+        case .photoLibraryAccessDenied:
+            "You need to allow access to your photo library to perform this action. Go to Settings > Image Filter > Photos and chooise the limited access or full access option."
         }
     }
 }
